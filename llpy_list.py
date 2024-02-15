eb9fb12ff70c3426f001626722f45e31dd3ca2bb
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-02-15 16:32:49 -0600

    Update linked-list example

--- llpy_list.py
+++ llpy_list.py
@@ -16,27 +16,21 @@ class List:
 @aot
 def testme(n: Int64) -> Int64:
     struct_sizeof = 16
-
-    head = _df.make(List,  next_ptr=pointer_api.alloc(struct_sizeof), data=123)
-    orig_head = head
-    node1 = _df.make(List, next_ptr=0, data=123 + n)
-    curnode = io.sync(
-        pointer_api.store(head.next_ptr, node1),
-        head,
-    )
-
+    null = pointer_api.as_pointer(0)
+    alloc = pointer_api.alloc(struct_sizeof)
+    head = _df.make(List,  next_ptr=alloc, data=123)
+    node1 = _df.make(List, next_ptr=null, data=123 + n)
+    pointer_api.store(head.next_ptr, node1)
+    curnode = head
     out = 0
     cont = True
+
     while cont:
-
-        out += curnode.data
-        cont = False
-
-        if curnode.next_ptr != 0:
+        out = out + curnode.data
+        cont = curnode.next_ptr != null
+        if cont:
             curnode = pointer_api.load(List, curnode.next_ptr)
-            cont = io.sync(curnode, True)
 
-
-    return io.sync(out, pointer_api.free(orig_head.next_ptr), out)
-
+    pointer_api.free(head.next_ptr)
+    return out
 
