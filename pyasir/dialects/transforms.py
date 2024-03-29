32835f06364796b6f11456ada9a2b93c23d1a8ab
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-03-29 13:26:59 -0500

    Add lift transformation

--- pyasir/dialects/transforms.py
+++ pyasir/dialects/transforms.py
@@ -3,20 +3,59 @@ from __future__ import annotations
 from dataclasses import dataclass, field
 from pprint import pprint
 from functools import singledispatch
-from typing import Any
+from typing import Any, Callable
 
 import pyasir.nodes as _df
 
 
+
+def lift(expr: _df.DFNode) -> tuple[_df.DFNode, dict[_df.ArgNode, _df.ArgNode]]:
+    """Lift expression to return a copy of the `expr` with all
+    argument node replaced with fresh ones.
+
+    Returns
+    -------
+    (result, arg_map)
+        result: The lifted nodes
+        arg_map: Mapping from old ArgNode to new ArgNode.
+    """
+    new_args = []
+    arg_map = {}
+
+    def transformer(node: _df.DFNode):
+        if isinstance(node, _df.ArgNode):
+            repl: _df.ArgNode = arg_map.get(node)
+            assert repl is not node
+            if repl is None:
+                repl = node.clone()
+                new_args.append(repl)
+                arg_map[node] = repl
+            return repl
+        else:
+            return None   # to descent
+
+    ctx = TransformerContext(transformer=transformer)
+    result = transform_visitor(expr, ctx)
+    return result, arg_map
+
+
 def transform(root: _df.FuncDef):
-    ctx = TransformerContext()
+    def transformer(node: _df.DFNode):
+        transform = getattr(node, "dialect_transform", None)
+        if transform is None:
+            return None
+        return transform()
+
+    ctx = TransformerContext(transformer=transformer)
     result = ctx.visit(root)
     return result
 
 
 @dataclass(frozen=True)
 class TransformerContext:
+    transformer: Callable[[_df.DFNode], _df.DFNode | None]
     repl_cache: dict[int, Any] = field(default_factory=dict)
+
 
     def visit(self, node: _df.FuncDef):
         return transform_visitor(node, self)
@@ -45,16 +84,13 @@ def _(node: tuple, ctx: TransformerContext):
 
 @transform_visitor.register
 def _(node: _df.DFNode, ctx: TransformerContext):
-    repl = {}
-    for name, child in node.get_child_nodes().items():
-        repl[name] = ctx.visit_child(child, name=name, parent=node)
-    return node.replace_child_nodes(repl)
-
-
-@transform_visitor.register
-def _(node: _df.DialectMixin, ctx: TransformerContext):
-    transform = getattr(node, "transform", None)
-    if transform is None:
-        raise TypeError("dialectic undefined", type(node))
-    return transform()
+    result = ctx.transformer(node)
+    if result is None:
+        # Recursively apply to children
+        repl = {}
+        for name, child in node.get_child_nodes().items():
+            repl[name] = ctx.visit_child(child, name=name, parent=node)
+        return node.replace_child_nodes(repl)
+    else:
+        return result
 
