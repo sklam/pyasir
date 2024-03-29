d32364307e7f21c8f32402fd00a8615d84041981
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2024-03-29 13:26:33 -0500

    Better error message from failing type_hints

--- pyasir/details/props.py
+++ pyasir/details/props.py
@@ -12,7 +12,11 @@ class NodeChildren(Property):
 def get_properties(node_type):
     mod = sys.modules[node_type.__module__]
     ns = mod.__dict__
-    hints = get_type_hints(node_type, ns, include_extras=True)
+    try:
+        hints = get_type_hints(node_type, ns, include_extras=True)
+    except Exception as e:
+        e.add_note(f"from get_type_hints({node_type}, {mod})")
+        raise
     props = {}
     for k, ty in hints.items():
         if isinstance(ty, _AnnotatedAlias):
